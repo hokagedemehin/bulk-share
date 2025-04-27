29316da6abca52926b21b941d4ce231429eaba51
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2025-04-28 00:59:45 +0100

    remove unwanted route

--- app/api/auth/[slug]/route.ts
+++
@@ -1,6 +0,0 @@
-// import { createAuthRouteHandlers } from "@/util/helpers/amplifyServerUtils";
-
-// export const GET = createAuthRouteHandlers({
-//   redirectOnSignInComplete: "/home",
-//   redirectOnSignOutComplete: "/sign-in",
-// });
